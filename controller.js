c6b45e098ad3e2006ecd280e65a3b3960d00ced4
apw516 <redhawk5665@gmail.com>
2022-04-18 23:38:42 +0700

    add controller to get mahasiswa with id

--- controller.js
+++ controller.js
@@ -2,6 +2,7 @@
 
 var response = require('./rest');
 var connection = require('./koneksi');
+const conn = require('./koneksi');
 
 exports.index = function(req,res){
     response.ok("Aplikasi rest api start",res)
@@ -16,4 +17,17 @@ exports.tampilsemuamahasiswa = function(req,res){
             response.ok(rows,res)
         }
     })
+}
+
+//menampilkan semua data mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function(req,res){
+    let id = req.params.id;
+    connection.query('select * from mahasiswa where id_mahasiswa = ?',[id],
+     function(error,rows,fields){
+         if(error){
+             connection.log(error)
+         }else{
+             response.ok(rows,res)
+         }
+     })
 }
