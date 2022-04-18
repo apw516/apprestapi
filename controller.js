afd920be4e85c053a34327103f85c915d3022957
apw516 <redhawk5665@gmail.com>
2022-04-18 23:21:40 +0700

    change controller.js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./rest');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Aplikasi rest api start")
+    response.ok("Aplikasi rest api start",res)
 }
