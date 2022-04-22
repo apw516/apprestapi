7657450c0e344a44da25fd34960d25cb2ed82ce3
apw516 <redhawk5665@gmail.com>
2022-04-22 21:33:51 +0700

    daftar routes index

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var morgan = require('morgan');
 const app = express();
 
 //parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(morgan('dev'));
 //memanggil routes.js
 var routes = require('./routes');
 routes(app);
+//daftarkan menu routes dari index
+app.use('/auth',require('./middleware'))
  app.listen(3000,() => {
-     console.log('server starter on port');
+     console.log('server starter on port 300');
  });
